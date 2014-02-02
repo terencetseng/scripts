c41afad8edb0963085d60f058f5b075b58b24824
Andre Brutus <andre@wepay.com>
2014-02-02 12:27:30 -0800

    Making the script executable

--- git-autocomplete.sh
+++ git-autocomplete.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p ~/bin/
 curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/git-completion.bash
+chmod +x ~/bin/git-completion.bash
 echo "if [ -f ~/bin/git-completion.bash ]; then
 	~/bin/git-completion.bash
 fi" >> ~/.bash_profile
