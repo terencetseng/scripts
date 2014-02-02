78b724b1b1bb170c1a08f3d0de7c16b34de9ea72
Andre Brutus <andre@wepay.com>
2014-02-02 12:07:31 -0800

    Fixed name

--- git-autocomplete.sh
+++ git-autocomplete.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/bin/
 curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/git-completion.bash
 echo "if [ -f ~/git-completion.bash ]; then
-	~/bin/.it-completion.bash
+	~/bin/git-completion.bash
 fi" >> ~/.bash_profile
 source ~/.bash_profile
