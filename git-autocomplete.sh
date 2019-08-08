0d74a68641aba0cb3cb25bc74b7493a4ef9a5d33
Terence Tseng <terence@wepay.com>
2019-08-08 11:07:17 -0700

    update path

--- git-autocomplete.sh
+++ git-autocomplete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/bin/
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/.git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/git-completion.bash
 chmod +x ~/bin/git-completion.bash
 echo "if [ -f ~/bin/git-completion.bash ]; then
 	~/bin/git-completion.bash
