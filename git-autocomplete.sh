e51cfff3a9e751b21e3624772e51de9dddf82acf
Terence Tseng <terence@wepay.com>
2019-08-08 11:17:43 -0700

    update bash script

--- git-autocomplete.sh
+++ git-autocomplete.sh
@@ -3,6 +3,6 @@ mkdir -p ~/bin/
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/git-completion.bash
 chmod +x ~/bin/git-completion.bash
 echo "if [ -f ~/bin/git-completion.bash ]; then
-	~/bin/git-completion.bash
+	source ~/bin/git-completion.bash
 fi" >> ~/.bash_profile
 source ~/.bash_profile
